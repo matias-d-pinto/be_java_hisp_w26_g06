b0553bce29005f8c416fc004c5ebfafd7c1ce11c
Matias Pinto <matiasdaniel.pinto@mercadolibre.com>
2024-05-06 13:10:46 -0300

    feat: validations on requests DTO
    Co-Authored-By: MarinaCaminosML <marina.caminos@mercadolibre.com>
    Co-Authored-By: bambricca <bruno.ambricca@mercadolibre.com>
    Co-Authored-By: jccaserio <164526556+jccaserio@users.noreply.github.com>
    Co-Authored-By: lucasezequielmartinez <lucas.lmartinez@mercadolibre.com>
    Co-Authored-By: tdonzis-meli <164518308+tdonzis-meli@users.noreply.github.com>

--- src/main/java/com/sprint/socialmeli/controller/PostsController.java
+++ src/main/java/com/sprint/socialmeli/controller/PostsController.java
@@ -3,13 +3,17 @@ package com.sprint.socialmeli.controller;
 import com.sprint.socialmeli.dto.post.FollowedProductsResponseDTO;
 import com.sprint.socialmeli.dto.post.PostDTO;
 import com.sprint.socialmeli.service.post.IPostService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
+@Validated
 public class PostsController {
 
     @Autowired
@@ -22,7 +26,7 @@ public class PostsController {
      * Calls postService to create new post
      */
     @PostMapping("/post")
-    public ResponseEntity<Integer> createPost(@RequestBody PostDTO post){
+    public ResponseEntity<Integer> createPost(@Valid @RequestBody PostDTO post){
         Integer idCreated = this.postService.createPost(post);
         return new ResponseEntity<>(idCreated, HttpStatus.OK);
     }
@@ -35,8 +39,11 @@ public class PostsController {
      * Calls postService to get the list of the posts
      */
     @GetMapping("/followed/{userId}/list")
-    public ResponseEntity<FollowedProductsResponseDTO> getFollowedPosts(@PathVariable Integer userId,
-                                                                        @RequestParam(required = false) String order){
+    public ResponseEntity<FollowedProductsResponseDTO> getFollowedPosts(
+            @PathVariable
+            @Positive(message = "El userId debe ser mayor a cero")
+            Integer userId,
+            @RequestParam(required = false) String order){
         return new ResponseEntity<>(postService.getFollowedProductsList(userId, order), HttpStatus.OK);
     }
 
--- src/main/java/com/sprint/socialmeli/exception/ExceptionController.java
+++ src/main/java/com/sprint/socialmeli/exception/ExceptionController.java
@@ -1,12 +1,15 @@
 package com.sprint.socialmeli.exception;
 
 import com.sprint.socialmeli.dto.ExceptionDto;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -36,9 +39,27 @@ public class ExceptionController {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> httpNotReadable(HttpMessageNotReadableException e) {
-        ExceptionDto exceptionDto = new ExceptionDto("Datos del vehículo mal formados o incompletos.");
+        ExceptionDto exceptionDto = new ExceptionDto("Json Parsing Error");
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> invalidArguments(ConstraintViolationException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getConstraintViolations()
+                .stream().findFirst().get().getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> httpNotReadable(NoResourceFoundException e) {
+        ExceptionDto exceptionDto = new ExceptionDto("El campo no puede estar vacío");
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> invalidArguments(MethodArgumentTypeMismatchException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
 
 }
