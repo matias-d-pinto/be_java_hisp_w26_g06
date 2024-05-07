2fefa30bfa8ab401f2888290384e14027be56a95
Matias Pinto <matiasdaniel.pinto@mercadolibre.com>
2024-05-07 18:06:57 -0300

    fix: tests on PostServiceImplTest
    Co-Authored-By: MarinaCaminosML <marina.caminos@mercadolibre.com>
    Co-Authored-By: bambricca <bruno.ambricca@mercadolibre.com>
    Co-Authored-By: jccaserio <164526556+jccaserio@users.noreply.github.com>
    Co-Authored-By: lucasezequielmartinez <lucas.lmartinez@mercadolibre.com>
    Co-Authored-By: tdonzis-meli <164518308+tdonzis-meli@users.noreply.github.com>

--- src/test/java/com/sprint/socialmeli/service/post/PostServiceImplTest.java
+++ src/test/java/com/sprint/socialmeli/service/post/PostServiceImplTest.java
@@ -23,6 +23,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,8 +49,9 @@ class PostServiceImplTest {
 
     PostResponseDTO postResponseDTO_1;
     PostResponseDTO postResponseDTO_2;
+    PostResponseDTO postResponseDTO_3;
     static List<Post> posts;
-    static List<Post> postsLastTwoWeeks;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @BeforeEach
     void setUp() {
@@ -68,68 +70,106 @@ class PostServiceImplTest {
                 new Seller(new User(3, "Francisco"))
         ));
 
-        postResponseDTO_1 = new PostResponseDTO(101, 1, "8-05-2024",
-                new ProductDTO(
-                        2,
-                        "Silla Interior",
-                        "Exterior",
-                        "Racer",
-                        "Red and Black",
-                        ""
-                ), 1, 220.8);
-        postResponseDTO_2 = new PostResponseDTO(101, 0, "10-05-2024",
-                        new ProductDTO(
-                                1,
-                                "Silla exterior",
-                                "Exterior",
-                                "Racer",
-                                "Red and Black",
-                                ""
-                        ), 1, 120.1);
+        postResponseDTO_1 = new PostResponseDTO(
+                101,
+                1,
+                formatter.format(LocalDate.now().minusWeeks(1).minusDays(2)),
+                ProductDTO.builder()
+                    .product_id(2)
+                    .product_name("Silla Interior")
+                    .type("Interior")
+                    .brand("Razer")
+                    .color("Red and black")
+                    .notes("")
+                .build(),
+                1,
+                220.8);
+
+        postResponseDTO_2 = new PostResponseDTO(101, 0,
+                formatter.format(LocalDate.now().minusWeeks(1).minusDays(1)),
+                ProductDTO.builder()
+                    .product_id(1)
+                    .product_name("Silla exterior")
+                    .type("Exterior")
+                    .brand("Razer")
+                    .color("Red and black")
+                    .notes("")
+                .build(),
+                1,
+                120.1);
+
+        postResponseDTO_3 = new PostResponseDTO(101, 2,
+                formatter.format(LocalDate.now().minusWeeks(1)),
+                ProductDTO.builder()
+                    .product_id(3)
+                    .product_name("Silla Gamer")
+                    .type("Gamer")
+                    .brand("Razer")
+                    .color("Red and black")
+                    .notes("")
+                .build(),
+                1,
+                300.9);
     }
 
     @BeforeAll
     static void setUpBeforeClass() {
         posts = List.of(
                 new Post(
-                        new Product(
-                                1,
-                                "Silla exterior",
-                                "Exterior",
-                                "Racer",
-                                "Red and Black",
-                                ""
-                        ), LocalDate.of(2024, 5,10), 1, 120.1
-                ),
+                    Product.builder()
+                        .id(1)
+                        .name("Silla exterior")
+                        .type("Exterior")
+                        .brand("Razer")
+                        .color("Red and black")
+                        .notes("")
+                    .build(),
+                    LocalDate.now().minusWeeks(1).minusDays(1),
+                    1,
+                    120.1),
                 new Post(
-                        new Product(
-                                2,
-                                "Silla Interior",
-                                "Exterior",
-                                "Racer",
-                                "Red and Black",
-                                ""
-                        ), LocalDate.of(2024, 5, 8), 1, 220.8
-                )
-        );
-        postsLastTwoWeeks = List.of(
+                    Product.builder()
+                        .id(2)
+                        .name("Silla Interior")
+                        .type("Interior")
+                        .brand("Razer")
+                        .color("Red and black")
+                        .notes("")
+                    .build(),
+                     LocalDate.now().minusWeeks(1).minusDays(2),
+                    1,
+                    220.8),
                 new Post(
-                        new Product(
-                                1,
-                                "Silla exterior",
-                                "Exterior",
-                                "Racer",
-                                "Red and Black",
-                                ""
-                        ), LocalDate.now().minusWeeks(1), 1, 120.1
-                )
+                    Product.builder()
+                        .id(3)
+                        .name("Silla Gamer")
+                        .type("Gamer")
+                        .brand("Razer")
+                        .color("Red and black")
+                        .notes("")
+                    .build(),
+                    LocalDate.now().minusWeeks(1),
+                    1,
+                    300.9),
+                new Post(
+                    Product.builder()
+                        .id(2)
+                        .name("Silla Interior")
+                        .type("Interior")
+                        .brand("Razer")
+                        .color("Red and black")
+                        .notes("")
+                    .build(),
+                    LocalDate.now().minusWeeks(3),
+                    1,
+                    120.1)
         );
     }
 
     // Order by date unit tests - T-0005 -----------------START
     // ---- Bad case ------
     @Test
-    @DisplayName("Date order type does not exist")
+    @DisplayName("Date order type 'asd' does not exist")
     void orderTypeDoesNotExists() {
         // Arrange
         String order = "asd"; // Random string
@@ -137,7 +177,8 @@ class PostServiceImplTest {
         // Act & assert
         Mockito.when(usersService.checkAndGetCustomer(customer.getUser().getUserId())).thenReturn(customer);
 
-        assertThrows(BadRequestException.class, () -> this.postService.getFollowedProductsList(customer.getUser().getUserId(), order));
+        assertThrows(BadRequestException.class, () ->
+                this.postService.getFollowedProductsList(customer.getUser().getUserId(), order));
     }
 
     // ---- Good case ------
@@ -159,7 +200,8 @@ class PostServiceImplTest {
         // Act
         Mockito.when(usersService.checkAndGetCustomer(customer.getUser().getUserId())).thenReturn(customer);
 
-        FollowedProductsResponseDTO response = this.postService.getFollowedProductsList(customer.getUser().getUserId(), order.toString());
+        FollowedProductsResponseDTO response = this.postService
+                .getFollowedProductsList(customer.getUser().getUserId(), order.toString());
 
         // Assert
         assertNotNull(response);
@@ -173,7 +215,7 @@ class PostServiceImplTest {
     void verifyCorrectOrderAscByDate() {
         // Arrange
         // Creating the possible response
-        List<PostResponseDTO> postsResponse = List.of(postResponseDTO_1, postResponseDTO_2);
+        List<PostResponseDTO> postsResponse = List.of(postResponseDTO_1, postResponseDTO_2, postResponseDTO_3);
 
         // Assert
         this.verifyCorrectOrderByDate(postsResponse, DateOrderType.DATE_ASC);
@@ -184,7 +226,7 @@ class PostServiceImplTest {
     void verifyCorrectOrderDescByDate() {
         // Arrange
         // Creating the possible response
-        List<PostResponseDTO> postsResponse = List.of(postResponseDTO_2, postResponseDTO_1);
+        List<PostResponseDTO> postsResponse = List.of(postResponseDTO_3, postResponseDTO_2, postResponseDTO_1);
 
         // Assert
         this.verifyCorrectOrderByDate(postsResponse, DateOrderType.DATE_DESC);
@@ -200,67 +242,31 @@ class PostServiceImplTest {
         Mockito.when(usersService.checkAndGetCustomer(customer.getUser().getUserId())).thenReturn(customer);
         Mockito.when(postRepository.findBySellerId(seller.getUser().getUserId())).thenReturn(posts);
 
-        FollowedProductsResponseDTO response = this.postService.getFollowedProductsList(customer.getUser().getUserId(), order.toString());
+        FollowedProductsResponseDTO response = this.postService
+                .getFollowedProductsList(customer.getUser().getUserId(), order.toString());
 
         // Assertion
-        assertEquals(posts.size(), response.getPosts().size());
+        assertEquals(posts.size() - 1, response.getPosts().size()); // -1 due to 1 being more than 2 weeks
         assertEquals(postsResponse, response.getPosts());
     }
     // -------------------------- T-0006 -------------------END
 
     // Last two weeks posts unit tests - T-0008 -----------------START
-
     @Test
     @DisplayName("Get posts from last two weeks")
     void getPostsFromLastTwoWeeks() {
         String order = "date_desc";
-
-        Mockito.when(usersService.checkAndGetCustomer(customer.getUser().getUserId())).thenReturn(customer);
-
+        List<PostResponseDTO> postsResponse = List.of(postResponseDTO_3, postResponseDTO_2, postResponseDTO_1);
         customer.setFollowed(List.of(1));
-
-        Mockito.when(postRepository.findBySellerId(seller.getUser().getUserId())).thenReturn(postsLastTwoWeeks);
 
         // Act
-        FollowedProductsResponseDTO response = this.postService.getFollowedProductsList(customer.getUser().getUserId(), order);
-
-        // Assert
-        assertEquals(postsLastTwoWeeks.size(), response.getPosts().size());
-
-    }
-
-    @Test
-    @DisplayName("NOT Get posts from beyond two weeks")
-    void notGetPostsFromBeyondTwoWeeks() {
-
-        String order = "date_desc";
-
-        List<Post> posts = List.of(
-                new Post(
-                        new Product(
-                                1,
-                                "Silla exterior",
-                                "Exterior",
-                                "Racer",
-                                "Red and Black",
-                                ""
-                        ), LocalDate.now().minusWeeks(3), 1, 120.1
-                )
-        );
-
         Mockito.when(usersService.checkAndGetCustomer(customer.getUser().getUserId())).thenReturn(customer);
-
-        customer.setFollowed(List.of(1));
-
         Mockito.when(postRepository.findBySellerId(seller.getUser().getUserId())).thenReturn(posts);
-
-        // Act
-        FollowedProductsResponseDTO response = this.postService.getFollowedProductsList(customer.getUser().getUserId(), order);
-
-
+        FollowedProductsResponseDTO response = this.postService
+                .getFollowedProductsList(customer.getUser().getUserId(), order);
         // Assert
-        assertEquals(0, response.getPosts().size());
-
+        assertEquals(3, response.getPosts().size());
+        assertEquals(postsResponse, response.getPosts());
     }
     // Last two weeks posts unit tests - T-0008 -----------------END
 
