23af842544f7979d66edf888693364b142069da0
jccaserio <juancruz.caserio@mercadolibre.com>
2024-05-07 09:48:06 -0300

    T-007

--- src/test/java/com/sprint/socialmeli/service/user/UsersServiceImplTest.java
+++ src/test/java/com/sprint/socialmeli/service/user/UsersServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.sprint.socialmeli.service.user;
 
+import com.sprint.socialmeli.dto.user.FollowerCountResponseDTO;
 import com.sprint.socialmeli.dto.user.FollowersResponseDTO;
 import com.sprint.socialmeli.dto.user.UserResponseDTO;
 import com.sprint.socialmeli.entity.Customer;
@@ -8,6 +9,7 @@ import com.sprint.socialmeli.entity.User;
 import com.sprint.socialmeli.exception.BadRequestException;
 import com.sprint.socialmeli.mappers.UserMapper;
 import com.sprint.socialmeli.repository.user.IUsersRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static com.sprint.socialmeli.mappers.UserMapper.mapUserToFollowerCountDto;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -103,4 +106,36 @@ class UsersServiceImplTest {
     }
     // Order by name unit tests - T-0003 -------------------END
 
+    @Test
+    @DisplayName("Get correct followers count")
+    public void geFollowersCountTest(){
+
+        //Arrange
+        //SETUP --> modularizar
+        Integer sellerId = 1;
+        Seller seller = new Seller(new User(sellerId, "Tesla X"));
+
+        List<Customer> customerList = List.of(
+                new Customer(new User(101, "Matias")),
+                new Customer(new User(102, "Nicolas"))
+        );
+
+        List<UserResponseDTO> usersDto = customerList
+                .stream()
+                .map(UserMapper::mapCustomerToUserResponseDto)
+                .toList();
+        //END SETUP
+
+        Mockito.when(usersRepository.findSellerById(sellerId)).thenReturn(seller);
+        Mockito.when(
+                usersRepository.findCustomerByPredicate(ArgumentMatchers.any())
+        ).thenReturn(customerList);
+        //Act
+        FollowerCountResponseDTO countDTO = usersService.getFollowersCount(sellerId);
+
+        FollowerCountResponseDTO expected = mapUserToFollowerCountDto(seller, 2);
+
+        //Assert
+        Assertions.assertEquals( expected, countDTO );
+    }
 }
